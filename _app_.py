c2b444119ec99a9f892939b84336935db04256e3
Ku-hsun <61910015+Ku-hsun@users.noreply.github.com>
2021-05-21 17:03:49 +0800

    Update _app_.py

--- _app_.py
+++ _app_.py
@@ -1,46 +1,51 @@
-from flask import Flask, request, abort
+import os
+from datetime import datetime
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+from flask import Flask, abort, request
+
+# https://github.com/line/line-bot-sdk-python
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('zN5Iy7Il6 / sI8 + F23sdopzWaWW / + PHsEGci9seuvObt0dB49EF8p7 / 3q3legaZ + gAqSldhMzdd2tLjeot4 / 9VzEJ + UghJU9Wspjx09PMRobhcPskg1jg6KHZNW1W1KWKNW1W1W1KWKNW1W1K1K1K1K1K1K1W1K1K1K1K1K1K1K1K1K1K1K1K6K1K1K1K1K1K1K1Z1E1E0E0E0C0W0W0W1W6K0K1W1K1K1K1K1E1E0E0')
-handler = WebhookHandler('13c23864d56dc3b5970a0bcc0d9da1a2')
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
 
-@app.route("/callback", methods=['POST'])
+@app.route("/", methods=["GET", "POST"])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    if request.method == "GET":
+        return "Hello Heroku"
+    if request.method == "POST":
+        signature = request.headers["X-Line-Signature"]
+        body = request.get_data(as_text=True)
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+        try:
+            handler.handle(body, signature)
+        except InvalidSignatureError:
+            abort(400)
 
-    return 'OK'
+        return "OK"
 
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
-
-
-if __name__ == "__main__":
-    app.run()
+    get_message = event.message.text
+    text_message = TextSendMessage(text='$ LINE emoji $')
+    if get_message == "長輩在不在?":
+        reply =ImageSendMessage(
+        original_content_url='https://github.com/Ku-hsun/Flask-LINE-Bot-Heroku/blob/main/img/FDA9DA75-9EF4-4B95-B03C-C9F8869A2339.jpg',
+        preview_image_url='https://github.com/Ku-hsun/Flask-LINE-Bot-Heroku/blob/main/img/FDA9DA75-9EF4-4B95-B03C-C9F8869A2339.jpg'
+        )
+    else:
+        reply =ImageSendMessage(
+        original_content_url='https://github.com/Ku-hsun/Flask-LINE-Bot-Heroku/blob/main/img/S__92119050.jpg',
+        preview_image_url='https://github.com/Ku-hsun/Flask-LINE-Bot-Heroku/blob/main/img/S__92119050.jpg'
+        )
+        
+    # Send To Line
+    # reply =TextSendMessage(text = fun1+'$ LINE 0x100001 $', emojis=[emoji])
+    line_bot_api.reply_message(event.reply_token, reply)
