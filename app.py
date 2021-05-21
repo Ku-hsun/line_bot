1e2bffe9c73590700ed9f1155b72d0bc155bf085
Ku-hsun <61910015+Ku-hsun@users.noreply.github.com>
2021-05-21 18:10:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
+# 載入需要的模組
 import os
 from datetime import datetime
-
 from flask import Flask, abort, request
-
 # https://github.com/line/line-bot-sdk-python
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
-
+# LINE 聊天機器人的基本資料
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
-
-
+# 接收 LINE 的資訊
 @app.route("/", methods=["GET", "POST"])
 def callback():
 
@@ -30,7 +28,14 @@ def callback():
 
         return "OK"
 
+# 新好友歡迎詞
+@handler.add(FollowEvent, )
+def first_message(event):
+    line_bot_api.reply_message(
+         event.reply_token,
+        TextSendMessage(text="歡迎加入~~~"))
 
+# 學你說話
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
