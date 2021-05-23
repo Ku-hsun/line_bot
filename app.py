677b322f662749866cc37e67304eaf04d7488b89
Ku-hsun <61910015+Ku-hsun@users.noreply.github.com>
2021-05-23 15:40:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,10 @@ def callback():
 # 學你說話
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=event.message.text))
         
     # Send To Line
     # reply =TextSendMessage(text = fun1+'$ LINE 0x100001 $', emojis=[emoji])
