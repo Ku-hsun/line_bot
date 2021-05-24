ef4740df9281e16d462503ddbcc3db421b151e22
Ku-hsun <61910015+Ku-hsun@users.noreply.github.com>
2021-05-24 17:10:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def handle_message(event):
                                       TextSendMessage(text=str(event.source.user_id)))
             line_bot_api.reply_message(
                 event.reply_token,
-                TextSendMessage(text="good afternoon"))
+                TextSendMessage(text=event.message.text))
         except:
             line_bot_api.reply_message(
                 event.reply_token,
