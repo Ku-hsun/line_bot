1adf22559f71711c1f42c75f4481d84fa1f7dfc3
Ku-hsun <61910015+Ku-hsun@users.noreply.github.com>
2021-06-11 18:07:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,25 +11,21 @@ app = Flask(__name__)
 # LINE 聊天機器人的基本資料
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+
+
 # 接收 LINE 的資訊
 @app.route("/", methods=["GET", "POST"])
 def callback():
-
     if request.method == "GET":
         return "Hello Heroku"
     if request.method == "POST":
         signature = request.headers["X-Line-Signature"]
         body = request.get_data(as_text=True)
-        print(body)
-
         try:
             handler.handle(body, signature)
         except InvalidSignatureError:
             abort(400)
-
         return "OK"
-
-
 
 # 收發訊息
 # 當收到 MessageEvent (信息事件)，而且信息是屬於 TextMessage (文字信息)的時候，就執行下列程式碼。
@@ -54,9 +50,6 @@ def handle_message(event):
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text='沒收到訊息再發送一次'))
-        
-    # Send To Line
-    # reply =TextSendMessage(text = fun1+'$ LINE 0x100001 $', emojis=[emoji])
-    line_bot_api.reply_message(event.reply_token, reply)
+
 if __name__ == "__main__":
     app.run()
