c31515e4b36d6bf04bf7a9bdf45f5d0d515e1e8a
Ku-hsun <61910015+Ku-hsun@users.noreply.github.com>
2021-06-18 09:21:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -18,21 +18,21 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
 
-# 圖片網址
-'''config = {
-  "apiKey": "AIzaSyALiO25iMMwqT7PJf_FqgX_7Y4L6MxQtMY",
-  "authDomain": "fast-mariner-312118.firebaseapp.com",
-  "databaseURL": "https://fast-mariner-312118-default-rtdb.asia-southeast1.firebasedatabase.app",
-  "projectId": "fast-mariner-312118",
-  "storageBucket": "fast-mariner-312118.appspot.com",
-  "messagingSenderId": "779980589461",
-  "appId": "1:779980589461:web:0d466ff5620c41c6c44e9c",
-  "measurementId": "G-1KZ5ZQVLYD"
+config = {
+    "apiKey": "AIzaSyALiO25iMMwqT7PJf_FqgX_7Y4L6MxQtMY",
+    "authDomain": "fast-mariner-312118.firebaseapp.com",
+    "databaseURL": "https://fast-mariner-312118-default-rtdb.asia-southeast1.firebasedatabase.app",
+    "projectId": "fast-mariner-312118",
+    "storageBucket": "fast-mariner-312118.appspot.com",
+    "messagingSenderId": "779980589461",
+    "appId": "1:779980589461:web:0d466ff5620c41c6c44e9c",
+    "measurementId": "G-1KZ5ZQVLYD"
 }
-while True:
+
+def saveupdata():
     # 存檔的時間
-    nowday = datetime.datetime.now().strftime('%Y_%m_%d')
-    deltime = (datetime.datetime.now() + datetime.timedelta(seconds=-8)).strftime('%Y_%m_%d_%H_%M_%S')
+    nowday = datetime.now().strftime('%Y_%m_%d')
+    deltime = (datetime.now() + timedelta(seconds=-8)).strftime('%Y_%m_%d_%H_%M_%S')
     # 上傳準備
     firebase = pyrebase.initialize_app(config)
     storage = firebase.storage()
@@ -42,14 +42,12 @@ while True:
         # 上傳 回存儲url
         url = storage.child(path_on_cloud).get_url(None)
         urllib.request.urlopen(url).read()
-        clourl = storage.child(path_on_cloud).get_url(None)
-        time.sleep(2)
+        random_img_url = storage.child(path_on_cloud).get_url(None)
+        sleep(8)
+        return random_img_url
     except:
-        pass'''
-# random_img_url = clourl
-while True:
-    random_img_url = 'https://miro.medium.com/max/12000/0*pc9GET-Mnc6G8CRJ'
-    sleep(5)
+        pass
+
 # 接收 LINE 的資訊
 @app.route("/", methods=["GET", "POST"])
 def callback():
@@ -72,13 +70,14 @@ def callback():
 def handle_message(event):
     while True:
         try:
+            random_img_url = 0
+            saveupdata()
             line_bot_api.push_message('U1110b9cf839a201aa15f37aaf5a71ea3',
                                       ImageSendMessage(
                                           original_content_url=random_img_url,
                                           preview_image_url=random_img_url))
-        except LineBotApiError as e:
-            # error handle
-            ...
+        except:
+            pass
 
 if __name__ == "__main__":
     app.run()
